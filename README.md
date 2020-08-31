2365e03b9de5266131b9500a7aaab9e32a887674
Simo Nkosi <wilzmar.kwd@gmail.com>
2020-09-01 01:39:05 +0200

    The final draft to my sketcpad

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is Simo's first project.
+In this project I’ll be creating a pretty grid to flex my DOM manipulation skills. I am going to build a browser version of something between a sketchpad and an Etch-A-Sketch.
---
+++ script.js
@@ -0,0 +1,57 @@
+const container = document.querySelector('#container');
+const startBtn = document.querySelector('#startGrid');
+const clearBtn = document.querySelector('#clearGrid');
+
+function fillGrid(inputValue) {
+    let squareHeight  = 100/inputValue + "vw";
+    let gridSize = inputValue ** 2;
+
+    for (let i = 0; i < gridSize; i++){
+        let square = document.createElement('div');
+        square.className = 'square';
+        square.style.height = squareHeight;
+        square.style.margin = "0";
+        square.style.border =  "1px solid darkgrey";
+        square.addEventListener('mouseenter', changeColor);
+        container.appendChild(square);  
+    }
+}
+
+function startGrid() {
+    let input = prompt("Enter a number between 1 and 64 to fill grid");
+
+    if(input < 1 || input > 64 || isNaN(input)) {
+        do {
+            alert("Please type a number between 1 and 64!")
+            input = prompt("Enter a number between 1 and 64 to fill grid");
+         } 
+         while (input < 1 || input > 64 || isNaN(input)); 
+    }
+
+    container.style.gridTemplateColumns = "repeat("+input+", 1fr)";
+    fillGrid(input);
+}
+
+function clearGrid() {
+    const grid = document.querySelectorAll('.square').forEach((square) => {
+        square.remove();
+    })
+}
+
+function randomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let color =  "rgb("+r+","+g+","+b+")";
+
+    return color;
+}
+
+function changeColor() {
+    if (event.target.style.background == "") {
+        event.target.style.background = randomColor();
+    }
+}
+
+startBtn.addEventListener("click", startGrid);
+clearBtn.addEventListener("click", clearGrid);
